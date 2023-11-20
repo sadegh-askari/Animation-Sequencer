cc8643eb32c8165cf7fd976b1b4b7a128c8ba56a
Sadegh Askari <sm.sadegh.askari@gmail.com>
2023-11-20 16:27:04 +0330

    fix for spine animation step

--- Scripts/Runtime/Core/Steps/SpineAnimationClipStep.cs
+++ Scripts/Runtime/Core/Steps/SpineAnimationClipStep.cs
@@ -10,7 +10,7 @@ namespace BrunoMikoski.AnimationSequencer
     public class SpineAnimationClipStep : GameObjectAnimationStep
     {
         //[SerializeField] private float _speed;
-        [SerializeField] private SkeletonAnimation  _skeletonAnimation;
+        [SerializeField] private SkeletonAnimation _skeletonAnimation;
 
         [SerializeField] private AnimationReferenceAsset _animationRefrence;
         [SerializeField] private int _loopCount;
@@ -18,13 +18,14 @@ namespace BrunoMikoski.AnimationSequencer
         public override string DisplayName => "Spine Animation Clip";
 
         public SkeletonAnimation SkeletonAnimation => _skeletonAnimation;
+        private bool _played = false;
 
         public override void AddTweenToSequence(Sequence animationSequence)
         {
             Tween tween = new CallbackTweenAction(PlayAnimation, PlayAnimation, StopAnimation).GenerateTween(duration);
             tween.SetLoops(_loopCount);
             tween.SetDelay(Delay);
-            
+
             Sequence sequence = DOTween.Sequence();
             sequence.Join(tween);
 
@@ -37,6 +38,10 @@ namespace BrunoMikoski.AnimationSequencer
         private void PlayAnimation()
         {
             bool loop = _loopCount != 0;
+            if (!loop && _played)
+                return;
+            
+            _played = true;
             _skeletonAnimation.AnimationState.SetEmptyAnimation(0, 0);
             TrackEntry entry = _skeletonAnimation.AnimationState.SetAnimation(0, _animationRefrence, loop);
         }
@@ -44,20 +49,21 @@ namespace BrunoMikoski.AnimationSequencer
 
         private void StopAnimation()
         {
+            _played = false;
             //_skeletonAnimation.AnimationState.SetEmptyAnimation(0, 0);
         }
-        
+
         public override void ResetToInitialState()
         {
             StopAnimation();
         }
-        
+
         public override string GetDisplayNameForEditor(int index)
         {
             string clipName = "Animation";
             if (_skeletonAnimation != null)
                 clipName = _skeletonAnimation.name;
-            
+
             return $"{index}. Play {clipName} Clip";
         }
     }
